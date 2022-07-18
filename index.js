a794a0af8533c8b03d4846842eba2e2b6b6c27dc
Ritika Bhosale <54773240+ritikabhosale@users.noreply.github.com>
2022-07-18 10:35:28 +0530

    Added styles for home-page and options-page.

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// const { startServer } = require('server');
-// const { createApp } = require('./src/app.js');
-
 const { createApp } = require("./src/app");
 
 const fs = require('fs');
@@ -8,8 +5,6 @@ const appConfig = {
   root: './public',
   usersData: './data/users.json'
 };
-
-// startServer(4444, createApp(appConfig, {}, {}, console.log, fs));
 
 const app = createApp(appConfig, {}, {}, console.log, fs);
 app.listen(8888, () => console.log('Listening on http://localhost:8888'));
